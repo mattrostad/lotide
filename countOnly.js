938e33c51c67dd81dce059268abd8590bd171388
Matt Rostad <matt@rostad.ca>
2023-02-06 20:07:58 -0800

    CountOnly Function

---
+++ countOnly.js
@@ -0,0 +1,13 @@
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
