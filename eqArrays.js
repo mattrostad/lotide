cd247669757e57603ae4c16cd347d5a26c21cf77
Matt Rostad <102119876+mattrostad@users.noreply.github.com>
2023-02-24 11:29:18 -0800

    Update eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,5 @@
-const assertEqual = require('./assertEqual');
-const tail = require('./tail');
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
