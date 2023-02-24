ff9b76f0814e347f3ff1dc965d1e16db7bc8270c
Matt Rostad <matt@rostad.ca>
2023-02-24 11:56:40 -0800

    middle tests

---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual')
+const middle = require('../middle.js')
+
+//TEST CODE
+console.log(middle([1,2,3,4,5,6,7]))
+console.log(middle([10,11,13,14]))
