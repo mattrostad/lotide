c29162e0b902145f18fc9580610bc83fd990db1a
Matt Rostad <matt@rostad.ca>
2023-02-24 00:28:09 -0800

    Head function with export added

--- head.js
+++ head.js
@@ -1,19 +1,8 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed:  ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-function head(array){
+const head = function (array){
   return array[0]
 }
 
-// TEST CODE
-
-
-
-assertEqual(head([5,6,7]), 5)
-assertEqual(head([10,11,12]), 11)
+module.exports = head;
