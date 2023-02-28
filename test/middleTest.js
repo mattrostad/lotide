569b8d233cfae7940ca396e21f4d1ec8118ddf92
Matt Rostad <matt@rostad.ca>
2023-02-27 19:10:49 -0800

    using mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,14 @@
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual')
+const assert = require('chai').assert;
 const middle = require('../middle.js')
 
-//TEST CODE
-console.log(middle([1,2,3,4,5,6,7]))
-console.log(middle([10,11,13,14]))
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.strictEqual(middle([1, 2, 3]), 2);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(middle(['5']), '5'); 
+  });
+  it("returns 2 for [1, 2, 3, 4]", () => {
+    assert.strictEqual(middle([1, 2, 3, 4]), 2);
+  });
+});
